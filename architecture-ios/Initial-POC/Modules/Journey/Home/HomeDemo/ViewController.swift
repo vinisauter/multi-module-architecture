57de61b4dee2e5554205d6a864929441f94ef940
Nykolas Mayko <nykolas.m.barbosa@next.b.br>
2021-11-08 19:44:25 -0300

    Creating the project using a xcworkspace for better organization

---
+++ architecture-ios/Initial-POC/Modules/Journey/Home/HomeDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeDemo
+//
+//  Created by Nykolas Mayko Maia Barbosa on 08/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ architecture-ios/Initial-POC/Modules/Structural/Networking/NetworkingInterfaces/HTTPClient.swift
@@ -0,0 +1,15 @@
+//
+//  HTTPClient.swift
+//  NetworkingInterfaces
+//
+//  Created by Nykolas Mayko Maia Barbosa on 08/11/21.
+//
+
+import Foundation
+
+public protocol HTTPClient {
+    func get() -> String
+    func post() -> String
+    func put() -> String
+    func delete() -> String
+}
