aed0baff47d5307d941a8a3fa5ba8bf718a18c5c
Marcos Roberto Perucelo <marcos.perucelo@bradesco.com.br>
2021-11-10 12:03:23 -0300

    Adiciona flutter ao projeto: implementação de multiple flutters e iniciando separação de módulos estrutural e negocial (usando profile).

--- architecture-android/app/src/main/java/com/example/app/MainActivity.kt
+++ architecture-android/app/src/main/java/com/example/app/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.example.app
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+private const val PROFILE_CLASSNAME = "com.example.profile.ProfileHomeActivity"
 
 class MainActivity : AppCompatActivity() {
+
+    private val moduleProfile by lazy { "profile" }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_profile.setOnClickListener {
+            onSuccessfulLoad(moduleProfile)
+        }
+    }
+
+    private fun onSuccessfulLoad(moduleName: String) {
+        when (moduleName) {
+            moduleProfile -> launchActivity(PROFILE_CLASSNAME)
+        }
+    }
+
+    private fun launchActivity(className: String) {
+        Intent().setClassName(BuildConfig.APPLICATION_ID, className)
+            .also {
+                startActivity(it)
+            }
     }
 }
