7d1d47c866b2cc83173676c179e9470b528f9c74
Luis Henrique Beltrao Santana <beltraoluis@brq.com>
2021-11-18 17:35:19 -0300

    add home

--- architecture-android/modules/journey/home/src/main/java/com/example/home/view/HomeFragment.kt
+++ architecture-android/modules/journey/home/src/main/java/com/example/home/view/HomeFragment.kt
@@ -1,12 +1,56 @@
 package com.example.home.view
 
+import android.net.Uri
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.FragmentExtensions.consume
+import androidx.fragment.app.FragmentExtensions.deeplink
+import androidx.fragment.app.viewModels
 import com.example.app.base.BaseLauncher
 import com.example.app.base.LauncherFragment
 import com.example.home.HomeLauncher
+import com.example.app.R
+import com.example.home.databinding.FragmentHomeBinding
+import com.example.home.viewmodel.HomeViewModel
 
 
 class HomeFragment : LauncherFragment() {
     override val launcher: BaseLauncher = HomeLauncher
+    private val viewModel: HomeViewModel by viewModels()
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        binding.viewModel = viewModel
+        return binding.root
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        consume(viewModel.btProfile) { success ->
+            if (success) {
+                Log.w("PRESSED", "profileButton")
+            }
+        }
+        consume(viewModel.btAgreement) { success ->
+            if (success) {
+                Log.w("PRESSED", "agreementButton")
+            }
+        }
+        consume(viewModel.btLogout) { success ->
+            if (success) {
+                val link = Uri.parse(getString(R.string.logout_deeplink))
+                deeplink(link)
+            }
+        }
+    }
 
     override fun execute() {}
 
--- architecture-android/modules/journey/login/src/main/java/com/example/login/view/LoginFragment.kt
+++ architecture-android/modules/journey/login/src/main/java/com/example/login/view/LoginFragment.kt
@@ -1,25 +1,22 @@
 package com.example.login.view
 
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.FragmentExtensions.consume
+import androidx.fragment.app.FragmentExtensions.dynamicNavigate
 import androidx.fragment.app.viewModels
 import com.example.app.base.LauncherFragment
 import com.example.login.LoginLauncher
 import com.example.login.databinding.FragmentLoginBinding
 import com.example.login.viewmodel.LoginViewModel
 
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
 class LoginFragment : LauncherFragment() {
     override val launcher = LoginLauncher
     private val viewModel: LoginViewModel by viewModels()
     private lateinit var binding: FragmentLoginBinding
+    private val goHome = LoginFragmentDirections.actionLoginFragmentToHomeNavigation().actionId
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -35,7 +32,7 @@ class LoginFragment : LauncherFragment() {
         super.onStart()
         consume(viewModel.btLogin) { success ->
             if (success) {
-                Log.w("PRESSED", "loginButton")
+                dynamicNavigate(goHome)
             }
         }
     }
