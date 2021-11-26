ff74ebe590d29193d2e15c6cdd25e018bd243bc4
vinicius.sauter <vinicius.sauter@hotmail.com>
2021-11-26 12:13:30 -0300

    activity navigation

--- architecture-android/app/src/main/java/com/example/app/AppNavigationActivity.kt
+++ architecture-android/app/src/main/java/com/example/app/AppNavigationActivity.kt
@@ -1,31 +1,15 @@
 package com.example.app
 
-import android.content.Context
-import androidx.annotation.IdRes
+import android.os.Bundle
 import com.core.base.NavigationActivity
-import com.core.extensions.clearStack
-import com.core.extensions.extra
-import com.core.extensions.start
 
-class AppNavigationActivity : NavigationActivity(R.navigation.app_navigation_graph) {
+class AppNavigationActivity : NavigationActivity(
+    graphResId = R.navigation.app_navigation_graph,
+    startDestination = R.id.splash_fragment
+) {
     val deepLink by lazy { intent.data }
-
-    override val startDestination: Int by lazy {
-        intent.getIntExtra(START_DESTINATION, DEFAULT_START_DESTINATION)
-    }
-
-    companion object {
-        private const val START_DESTINATION = "start_destination"
-        fun open(
-            context: Context,
-            // TODO: Replace IdRes with Enumerated Annotations?
-            @IdRes startDestination: Int = DEFAULT_START_DESTINATION,
-            clearStack: Boolean = false
-        ) {
-            context.start<AppNavigationActivity> {
-                extra(START_DESTINATION, startDestination)
-                if (clearStack) clearStack()
-            }
-        }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //TODO handle deepLink
     }
 }
