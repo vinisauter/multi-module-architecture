3323d0c7d5098df5e905538ca57bc0e08ecc3fdf
vinicius.sauter <vinicius.sauter@hotmail.com>
2021-12-28 14:43:26 -0300

    Structural Provider Reference

--- architecture-android/app/src/main/java/com/example/structural/networkingsecurev2/NetworkingSecureProviderV2.kt
+++ architecture-android/app/src/main/java/com/example/structural/networkingsecurev2/NetworkingSecureProviderV2.kt
@@ -1,7 +1,8 @@
 package com.example.structural.networkingsecurev2
 
+import android.app.Application
 import com.example.networking.RequestExecutor
 
 interface NetworkingSecureProviderV2 {
-    fun executor(): RequestExecutor
+    fun executor(application: Application): RequestExecutor
 }
