36819f8de384ae05ff9fe0d778e9702b934812a0
Ailton Vieira Pinto Filho <ailtonvivaz@brq.com>
2022-01-18 15:31:55 -0300

    Add FlutterModule to iOS project

---
+++ architecture-ios/Initial-POC/Modules/Structural/FlutterModule/FlutterModule/FlutterModule.h
@@ -0,0 +1,18 @@
+//
+//  FlutterModule.h
+//  FlutterModule
+//
+//  Created by Ailton Vieira Pinto Filho on 18/01/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FlutterModule.
+FOUNDATION_EXPORT double FlutterModuleVersionNumber;
+
+//! Project version string for FlutterModule.
+FOUNDATION_EXPORT const unsigned char FlutterModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlutterModule/PublicHeader.h>
+
+
---
+++ architecture-ios/Initial-POC/Modules/Structural/FlutterModule/FlutterModuleInterfaces/FlutterModuleInterfaces.h
@@ -0,0 +1,18 @@
+//
+//  FlutterModuleInterfaces.h
+//  FlutterModuleInterfaces
+//
+//  Created by Ailton Vieira Pinto Filho on 18/01/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FlutterModuleInterfaces.
+FOUNDATION_EXPORT double FlutterModuleInterfacesVersionNumber;
+
+//! Project version string for FlutterModuleInterfaces.
+FOUNDATION_EXPORT const unsigned char FlutterModuleInterfacesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlutterModuleInterfaces/PublicHeader.h>
+
+
