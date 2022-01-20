42f0afa06fde0ac26bf0a19fb8971cfcc18d35eb
Luis Henrique Beltrao Santana <beltraoluis@brq.com>
2022-01-20 17:19:36 -0300

    remove internal reference to extracted libs

--- architecture-android/engineering/interfaces/networking-interface/src/main/java/com/example/networking/RequestExecutor.kt
+++
@@ -1,8 +0,0 @@
-package com.example.networking
-
-interface RequestExecutor {
-    suspend fun get(): String
-    suspend fun post(): String
-    suspend fun put(): String
-    suspend fun delete(): String
-}
