07caf5aa8c135a8c820768a7cb760c0120ce8024
Ailton Vieira Pinto Filho <ailtonvivaz@outlook.com>
2022-01-29 13:22:24 -0300

    Add script to build flutter modules to android

--- architecture-flutter/README.md
+++ architecture-flutter/README.md
@@ -9,3 +9,5 @@ Caso queira atualizar apenas 1 módulo, passe o nome do módulo como
 ```sh
 sh build.sh task
 ```
+
+Existem também, uma opção para tirar a dependência do Flutter dos módulos, uma vez que irá usar o módulo estrutural: `--separate-flutter`
--- architecture-flutter/build.sh
+++ architecture-flutter/build.sh
@@ -3,19 +3,39 @@
 LIBFOLDER="${PWD}/build"
 SUBFOLDER="${LIBFOLDER}/host/outputs/repo"
 
-pushd "modules"
+pushd "modules" > /dev/null
 
     function build_lib() {
-        pushd "${directory}"
+        pushd "${directory}" > /dev/null
             echo "Building ${PWD}"
             echo "Module will be save in: {$LIBFOLDER}"
 
             flutter build aar --output-dir=${LIBFOLDER}
-            #echo "$d"
-        popd
+        popd > /dev/null
     }
 
-    MODULE=$1
+    for i in "$@"; do
+        case $i in
+            -h|--help)
+                echo "Usage: build.sh [MODULE] --separate-flutter"
+                echo "Builds the specified module and copies it to the build folder."
+                echo "If --separate-flutter is specified, the flutter pom will be split in a separate folder."
+                exit 0
+                ;;
+            --separate-flutter)
+                SEPARATE_FLUTTER=true
+                ;;
+            *)
+                if [ -n "$MODULE_DEFINED" ]; then
+                    echo "Unknown option ${i}"
+                    exit 1
+                fi
+        
+                MODULE=$i
+                ;;
+        esac
+        MODULE_DEFINED=true
+    done
 
     if [ -z "$MODULE" ]
     then
@@ -30,7 +50,19 @@ pushd "modules"
         build_lib
     fi
 
-popd
+popd > /dev/null
+
+if [ "${SEPARATE_FLUTTER}" = "true" ]; then
+    pushd "${SUBFOLDER}" > /dev/null
+        echo "POMS in ${PWD}"
+        if [ -z "$MODULE" ]
+        then
+            find . -iname "*.pom" -not -path "*/flutter_module/*" -exec sed -i '' -e '/<dependency>/,/<\/dependency>/d' {} \;
+        else
+            find . -iname "*.pom" -path "*/${MODULE}_business_module/*" -exec sed -i '' -e '/<dependency>/,/<\/dependency>/d' {} \;
+        fi
+    popd > /dev/null
+fi
 
 find build/host/outputs/repo -mindepth 1 -maxdepth 1 -type d -exec rsync -a {} build/ \;
 rm -rf build/host/
