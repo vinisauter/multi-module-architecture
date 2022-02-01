11dca307ff27dfa23ad9693c509eb4f11524e04f
Vinicius Sauter <vinicius.sauter@bradesco.com.br>
2022-02-01 17:09:59 -0300

    TestScope

--- architecture-android/modules/journey/login/src/test/java/com/example/login/presentation/LoginFragmentViewModelTest.kt
+++ architecture-android/modules/journey/login/src/test/java/com/example/login/presentation/LoginFragmentViewModelTest.kt
@@ -2,21 +2,45 @@ package com.example.login.presentation
 
 import android.app.Application
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
 import androidx.lifecycle.SavedStateHandle
+import com.core.extensions.State
 import com.example.journey.login.tracking.LoginTracking
 import com.example.tagging.TaggingExecutor
-import org.junit.After
-import org.junit.Before
-import org.junit.Rule
-import org.junit.Test
+import io.mockk.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.*
+import org.junit.*
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
+import org.mockito.kotlin.any
+import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.mock
+import org.mockito.kotlin.stub
 
+@ExperimentalCoroutinesApi
 @RunWith(JUnit4::class)
 internal class LoginFragmentViewModelTest {
     @get:Rule
     var rule = InstantTaskExecutorRule()
+
+    companion object {
+        private val testDispatcher by lazy { UnconfinedTestDispatcher() }
+        private val testScope by lazy { TestScope(testDispatcher) }
+
+        @JvmStatic
+        @BeforeClass
+        fun prepare() {
+            Dispatchers.setMain(testDispatcher)
+        }
+
+        @JvmStatic
+        @AfterClass
+        fun cleanup() {
+            Dispatchers.resetMain()
+        }
+    }
 
     //MOCK
     lateinit var useCase: LoginFragmentUseCase
@@ -39,10 +63,18 @@ internal class LoginFragmentViewModelTest {
     }
 
     @Test
-    fun `on login change loader state`() {
-        // GIVEN
+    fun `onLoginClicked() - change loader state`() = testScope.runTest {
+        //GIVEN
+        val observer = mockk<Observer<State>> { every { onChanged(any()) } just Runs }
+        vm.onStateChanged.observeForever(observer)
+        useCase.stub { onBlocking { useCase.login(any(), any()) }.doReturn(true) }
         // WHEN
         vm.onLoginClicked()
         // THEN
+        verifySequence {
+            observer.onChanged(State.Idle)
+            observer.onChanged(State.Running)
+            observer.onChanged(State.Idle)
+        }
     }
 }
