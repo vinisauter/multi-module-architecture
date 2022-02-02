1482800168629a293a955145c597cda33400066d
Tília Nogueira <t.salomao.nogueira@avanade.com>
2022-02-02 16:36:00 -0300

    utilizando runTest no lugar de runBlocking

--- architecture-android/modules/journey/login/src/test/java/com/example/login/business/repository/local/LoginStorageTest.kt
+++ architecture-android/modules/journey/login/src/test/java/com/example/login/business/repository/local/LoginStorageTest.kt
@@ -1,17 +1,25 @@
 package com.example.login.business.repository.local
 
 
+import com.example.login.InstantExecutorRule
 import com.example.storage.StorageExecutor
 import io.mockk.*
 import junit.framework.TestCase
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runTest
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 
+@ExperimentalCoroutinesApi
 @RunWith(JUnit4::class)
 internal class LoginStorageTest : TestCase() {
+
+    @get:Rule
+    val rule = InstantExecutorRule()
 
     //mockar storage executor
     private val executor = mockk<StorageExecutor>(relaxed = true)
@@ -29,7 +37,7 @@ internal class LoginStorageTest : TestCase() {
     fun `should save string when receives string`() {
 
         //estamos executando uma coroutine por isso usamos no runblock
-        runBlocking { storage.save("name") }
+        rule.testScope.runTest { storage.save("name") }
 
         //verificação
         coVerify {
--- architecture-android/modules/journey/login/src/test/java/com/example/login/business/repository/remote/LoginApiTest.kt
+++ architecture-android/modules/journey/login/src/test/java/com/example/login/business/repository/remote/LoginApiTest.kt
@@ -1,38 +1,46 @@
 package com.example.login.business.repository.remote
 
+import com.example.login.InstantExecutorRule
 import com.example.networking.RequestExecutor
 import io.mockk.coEvery
 import io.mockk.coJustRun
 import io.mockk.coVerify
 import io.mockk.mockk
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runTest
 import org.junit.Assert.*
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 
+@ExperimentalCoroutinesApi
 @RunWith(JUnit4::class)
 internal class LoginApiTest {
 
-    private lateinit var api : LoginApi
+    @get:Rule
+    val rule = InstantExecutorRule()
+
+    private lateinit var api: LoginApi
 
     private val unsecureRequestExecutor = mockk<RequestExecutor>()
     private val secureRequestExecutor = mockk<RequestExecutor>()
 
     @Before
-    fun setup(){
+    fun setup() {
         api = LoginApi(unsecureRequestExecutor, secureRequestExecutor)
     }
 
 
     @Test
-    fun `should return user when login request`(){
+    fun `should return user when login request`() {
         val expected = "João da Silva"
 
         coJustRun { unsecureRequestExecutor.get() }
 
-        runBlocking {
+        rule.testScope.runTest {
             val result = api.login("user", "password")
             assertEquals(expected, result)
         }
@@ -43,7 +51,6 @@ internal class LoginApiTest {
 
 
     }
-
 
 
 }
